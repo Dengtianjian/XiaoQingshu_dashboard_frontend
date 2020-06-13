0cd544fef202775c807ae312e28e044b17782083
DengTianjian <mail@isdtj.com>
2020-06-13 23:11:52 +0800

    vuex增加用户组存储

--- store/user.js
+++ store/user.js
@@ -1,12 +1,22 @@
+import Tools from "../functions/tools";
+
 export const state = () => ({
-  user_uid: null,
-  token: null
+  token: null,
+  userGroup: []
 });
 
 export const mutations = {
   updateUser(state, user) {
     state.user = user;
-    state.user_id=user.user_id;
-    state.token=user.token;
+    state.user_id = user.user_id;
+    state.token = user.token;
+  },
+  updateUserGroup(state, group) {
+    let type = Tools.getType(group);
+    if (type == "Array") {
+      state.userGroup.push(...group);
+    } else if (type == "Object") {
+      state.userGroup.push(group);
+    }
   }
 };
