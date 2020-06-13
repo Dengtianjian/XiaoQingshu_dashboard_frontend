e173733cea2958b4020c91e651fd80a892fe0094
DengTianjian <mail@isdtj.com>
2020-06-13 23:13:59 +0800

    增加 添加用户页面

--- pages/user/add.vue
+++ pages/user/add.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="userAdd">
-    <el-form :model="userForm" class="user-form">
-      <el-form-item label="头像" label-width="50">
+    <el-form
+      :model="userForm"
+      class="user-form"
+      label-width="80px"
+      label-position="left"
+    >
+      <el-form-item label="头像">
         <el-upload
           class="user-form-avatar-button"
           action=""
@@ -9,31 +14,47 @@
           :before-upload="uploadUserAvatar"
           :limit="1"
           :show-file-list="false"
+          label-position="top"
         >
           拖拽文件到这里或者点击这里上传
         </el-upload>
         <img
           class="user-form-avatar-preview"
           :src="userForm.avatarUrl"
-          :show="userForm.avatarUrl"
+          v-show="userForm.avatarUrl"
           wdith="100px"
         />
       </el-form-item>
-      <el-form-item label="用户名" label-width="50">
+      <el-form-item label="用户名" required>
         <el-input v-model="userForm.username" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="密码" label-width="50">
+      <el-form-item label="密码" required>
         <el-input v-model="userForm.password" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="昵称" label-width="50">
+      <el-form-item label="昵称" required>
         <el-input v-model="userForm.nickname" autocomplete="off"></el-input>
       </el-form-item>
+      <el-form-item label="用户组" required>
+        <el-select v-model="userForm.group" placeholder="请选择">
+          <el-option
+            v-for="item in group"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-button type="primary" @click="saveUser">保存</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
 export default {
+  beforeMount() {
+    this.group = this.$store.state.user.userGroup;
+  },
   data() {
     return {
       users: [],
@@ -43,8 +64,10 @@ export default {
         avatarUrl: "",
         username: "",
         password: "",
-        nickname: ""
-      }
+        nickname: "",
+        group: ""
+      },
+      group: []
     };
   },
   methods: {
@@ -65,6 +88,29 @@ export default {
           uploadLoading.close();
         });
       return false;
+    },
+    saveUser() {
+      let form = this.userForm;
+      let userData = {
+        avatar_url: form.avatar,
+        group: form.group,
+        nickname: form.nickname,
+        username: form.username,
+        password: form.password
+      };
+      if (!form.nickname || !form.username || !form.password || !form.group) {
+        this.$message({
+          message: "请输入必填写",
+          type: "warning"
+        });
+        return;
+      }
+      this.$http.post("user", "saveUser", userData).then(res => {
+        this.$message({
+          message: "添加成功",
+          type: "success"
+        });
+      });
     }
   }
 };
@@ -78,8 +124,8 @@ export default {
   display: inline-block;
   margin-left: 20px;
   width: 180px;
-  border:2px solid #eee;
-  border-radius:4px;
+  border: 2px solid #eee;
+  border-radius: 4px;
   box-sizing: border-box;
 }
 </style>
